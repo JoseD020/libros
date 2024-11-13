ae6f654e77c3f2ee033d4d49a5d343a354f1ebba
jinsigna15 <jinsigna15@cuc.edu.co>
2024-11-13 10:00:08 -0500

    CRUD - listado libros

--- src/app/componentes/libros/LibroListar.tsx
+++ src/app/componentes/libros/LibroListar.tsx
@@ -1,40 +1,56 @@
+import { useState } from "react";
+
+import { Libro } from "../../modelos/Libro";
+import { ARREGLO_LIBROS } from "../../mocks/Libro-mocks";
+import { ARREGLO_LIBROS_GENEROS } from "../../utilidades/dominios/DomGenero";
+
 export const LibroListar = () => {
+  const [arrLibros] = useState<Libro[]>(ARREGLO_LIBROS);
+
+  const obteneNombre = (valor: string) => {
+    for (const objGen of ARREGLO_LIBROS_GENEROS) {
+      if (objGen.codGenero == valor) {
+        return objGen.nombreGenero;
+      }
+    }
+  };
+
   return (
     <>
       <div className="pt-4 d-flex justify-content-center">
         <div className="col-md-10">
-          <table className="table table-info">
+          <table className="table table-bordered">
             <thead>
               <tr>
-                <th style={{width: "5%"}}>No.</th>
-                <th style={{width: "35%"}}>Titulo del Libro</th>
-                <th style={{width: "30%"}}>Autor</th>
-                <th style={{width: "15%"}} className="text-center">ISBN</th>
-                <th style={{width: "15%"}}>Editorial</th>
+                <th style={{ width: "5%" }}>Código</th>
+                <th style={{ width: "35%" }}>Titulo del Libro</th>
+                <th style={{ width: "30%" }}>Autor</th>
+                <th style={{ width: "15%" }} className="text-center">
+                  Género
+                </th>
+                <th style={{ width: "15%" }}>Imagen</th>
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <th scope="row">1</th>
-                <td>Cien años de soledad</td>
-                <td>Gabriel Garcia Marquez</td>
-                <td className="text-center">7788888</td>
-                <td>Sudamericana</td>
-              </tr>
-              <tr>
-                <th scope="row">2</th>
-                <td>La casa de los espiritus</td>
-                <td>Isabela Allende</td>
-                <td className="text-center">7778888888</td>
-                <td>Rama</td>
-              </tr>
-              <tr>
-                <th scope="row">3</th>
-                <td>El tunel</td>
-                <td>Ernesto Sabato</td>
-                <td className="text-center">774354588888</td>
-                <td>Planeta</td>
-              </tr>
+              {arrLibros.map((miLib: Libro) => (
+                <tr>
+                  <td>{miLib.codLibro}</td>
+                  <td>{miLib.tituloLibro}</td>
+                  <td>{miLib.autorLibro}</td>
+                  <td className="text-center">
+                    {obteneNombre(miLib.codGeneroLibro)}
+                  </td>
+                  <td>
+                    <img
+                      src={miLib.imagenLibroBase64}
+                      alt="la imagen"
+                      className="imagenListado"
+                    />
+                    <br />
+                    {miLib.imagenLibro}
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
---
+++ src/app/utilidades/dominios/DomGenero.tsx
@@ -0,0 +1,10 @@
+import { LibroGenero } from "../../modelos/LibroGenero";
+
+export const ARREGLO_LIBROS_GENEROS:Array<LibroGenero> =[
+    {codGenero: "1", nombreGenero: "Drama"},
+    {codGenero: "2", nombreGenero: "Ficción"},
+    {codGenero: "3", nombreGenero: "Infantiles"},
+    {codGenero: "4", nombreGenero: "Historicos"},
+    {codGenero: "5", nombreGenero: "Misterio"},
+    {codGenero: "6", nombreGenero: "Terror"},
+];
